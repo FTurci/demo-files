51b2a993132c10df75abfd8eca74b6efca5d25bc
Francesco Turci <turci@me.com>
2023-11-08 06:44:50 +0000

    chg

--- generators/dummy.py
+++ generators/dummy.py
@@ -8,7 +8,7 @@ script_dir = os.path.dirname(__file__)
 
 x = np.linspace(0,10)
 # x = np.sin(2*np.pi*t/3)+np.random.uniform(-0.1,0.1,len(t))
-U =  0.5*x**3-4*x**2+x+np.random.uniform(-8,8,len(x))
+U =  0.5*x**3-4*x**2+x+np.random.uniform(-8,9,len(x))
 
 np.savetxt("../csv/poly.csv",list(zip(x,U)),header="x,U",delimiter=",",comments="")
 import matplotlib.pyplot as plt
