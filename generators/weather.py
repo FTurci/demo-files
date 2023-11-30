223011886a4709043108d4fecbb293b33f6f6363
Francesco Turci <turci@me.com>
2023-11-30 17:02:42 +0000

    lectre9

--- generators/weather.py
+++ generators/weather.py
@@ -4,10 +4,10 @@ with open("../csv/weather.csv","w") as fout:
     fout.write("weather,\n")
     for i in range(100):
         if rng.uniform() <0.1:
-            fout.write(f"sunny,\n")
+            fout.write(f"sunny\n")
         elif rng.uniform()<0.5:
-            fout.write(f"cloudy,\n")
+            fout.write(f"cloudy\n")
         elif rng.uniform()<0.8:
-            fout.write(f"rainy,\n")
+            fout.write(f"rainy\n")
         else:
-            fout.write(f"windy,\n")
+            fout.write(f"windy\n")
